e251fdf65e507dc772f325215250c5abcaec038f
Daniil Rose <daniil.rose@posteo.org>
2023-08-09 23:13:51 -0400

    Add copilot back

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -9,12 +9,22 @@ return {
 		{ "hrsh7th/cmp-emoji" },
 		{ "hrsh7th/cmp-cmdline" },
 		{ "windwp/nvim-autopairs", opts = {} },
+		{ "zbirenbaum/copilot.lua" },
+		{ "zbirenbaum/copilot-cmp" },
 	},
 	config = function()
 		local cmp = require("cmp")
 
+		require("copilot").setup({
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+		})
+
+		require("copilot_cmp").setup()
+
 		cmp.setup({
 			sources = {
+				{ name = "copilot", group_index = 2 },
 				{ name = "nvim_lsp", group_index = 2 },
 				{ name = "path", group_index = 2 },
 				{ name = "luasnip", group_index = 2 },
